27819ae206c4cbaca959728b98e988e7b9f37de1
Quentiel <danielmp168@gmail.com>
2016-08-16 15:36:31 -0500

    Update Ejercicio-1_BLUE.js

--- rv/Ejercicio-1_BLUE.js
+++ rv/Ejercicio-1_BLUE.js
@@ -2,4 +2,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var contexto = miCanvas.getContext("2d");
 contexto.fillStyle="blue";
-contexto.fillRect(20,20,150,100);
+contexto.fillRect(100,100,150,100);
