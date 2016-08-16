e589a975fd731b6c707598bffb38f010e0bf4b85
Quentiel <danielmp168@gmail.com>
2016-08-16 15:38:38 -0500

    Update Ejercicio-1_GREEN.js

--- rv/Ejercicio-1_GREEN.js
+++ rv/Ejercicio-1_GREEN.js
@@ -2,4 +2,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var contexto = miCanvas.getContext("2d");
 contexto.fillStyle="green";
-contexto.fillRect(80,80,60,60);
+contexto.fillRect(200,200,90,60);
