d71d70a46aa46167d3ddcbea64052a471da4f01e
Quentiel <danielmp168@gmail.com>
2016-08-18 15:50:25 -0500

    Create ejercicio-4_cilindro.js

---
+++ rv/ejercicio-4_cilindro.js
@@ -0,0 +1,21 @@
+var escena = new THREE.Scene();
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                    	window.innerHeight*.95);
+			
+document.body.appendChild (renderizador.domElement);
+
+var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var cilindro = new THREE.Mesh( geometry, material );
+
+
+cilindro.rotateX(-Math.PI/1); 
+cilindro.rotateY(Math.PI/1);
+scene.add( cylinder );
+
+renderizador.render(escena,camara);
