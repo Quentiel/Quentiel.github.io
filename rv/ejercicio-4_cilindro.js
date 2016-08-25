16c4e60dfb755b8289ec3097a6324217b3953e07
Quentiel <danielmp168@gmail.com>
2016-08-24 23:43:26 -0500

    Update ejercicio-4_cilindro.js

--- rv/ejercicio-4_cilindro.js
+++ rv/ejercicio-4_cilindro.js
@@ -14,14 +14,15 @@ document.body.appendChild (renderizador.domElement);
 var geometry = new THREE.CylinderGeometry( 1, 1, 12, 30 );
 var material = new THREE.MeshBasicMaterial({color: 0x8B4513});
 var cilindro = new THREE.Mesh(geometry, material);
-
 cilindro.rotateX(-Math.PI/4); 
 cilindro.rotateY(Math.PI/5);
 escena.add(cilindro);
 
-var geometry1 = new THREE.SphereGeometry( 1, 1, 32 );
+var geometry1 = new THREE.SphereGeometry( 2, 2, 32 );
 var material1 = new THREE.MeshBasicMaterial( {color: 0x008000} );
 var sphere = new THREE.Mesh( geometry1, material1 );
+sphere.rotateX(-Math.PI/4); 
+sphere.rotateY(Math.PI/5);
 escena2.add( sphere );
 
 renderizador.render(escena,camara);
