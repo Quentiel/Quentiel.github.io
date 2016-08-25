758fae9d46ee62b59788e92450373223fef1e3a0
Quentiel <danielmp168@gmail.com>
2016-08-24 23:32:57 -0500

    Update ejercicio-4_cilindro.js

--- rv/ejercicio-4_cilindro.js
+++ rv/ejercicio-4_cilindro.js
@@ -11,7 +11,7 @@ renderizador.setSize( window.innerHeight*.95,
 document.body.appendChild (renderizador.domElement);
 
 var geometry = new THREE.CylinderGeometry( 1, 1, 12, 30 );
-var material = new THREE.MeshNormalMaterial();
+var material = new THREE.MeshNormalMaterial({color: 0xffff00});
 var cilindro = new THREE.Mesh(geometry, material);
 
 
