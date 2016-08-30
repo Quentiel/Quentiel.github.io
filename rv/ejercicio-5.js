99372a042c9489dec2891248af8dc92e69dc6cec
Quentiel <danielmp168@gmail.com>
2016-08-30 15:16:06 -0500

    Update ejercicio-5.js

--- rv/ejercicio-5.js
+++ rv/ejercicio-5.js
@@ -21,4 +21,5 @@ camara.position.z=5;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
-document.body.appendChild (renderizador
+document.body.appendChild (renderizador.domElement);
+renderizador.render ( escena, camara);
