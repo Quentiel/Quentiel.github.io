477efbebff072ccbe71ad870e6bf2011f081f11e
Quentiel <danielmp168@gmail.com>
2016-08-30 15:06:32 -0500

    Update ejemplo9-union.js

--- rv/ejemplo9-union.js
+++ rv/ejemplo9-union.js
@@ -21,7 +21,7 @@ escena.add(arbolMalla);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 
-renderizador = new THREE.WebGLREnderer();
+var renderizador = new THREE.WebGLREnderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild (renderizador.domElement);
 renderizador.render ( escena, camara);
