de13abbcda1360b5a9dc6d687ddc3d6d43a077af
Quentiel <danielmp168@gmail.com>
2016-08-30 15:29:13 -0500

    Update ejercicio-5.js

--- rv/ejercicio-5.js
+++ rv/ejercicio-5.js
@@ -1,4 +1,4 @@
-var troncoForma = new THREE.TorusGeometry(20, 2, 16, 100);
+var troncoForma = new THREE.TorusGeometry(20, 0.5, 16, 100);
 var esferaForma = new THREE.SphereGeometry(0.65);
 esferaForma.translate(0,1,0);
 
