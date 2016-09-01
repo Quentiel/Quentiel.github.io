8da09707ac6d8fe3301677b0917b9e6fbb8d719f
Quentiel <danielmp168@gmail.com>
2016-09-01 15:29:36 -0500

    Update ejercicio-5.js

--- rv/ejercicio-5.js
+++ rv/ejercicio-5.js
@@ -1,10 +1,10 @@
 var troncoForma = new THREE.TorusGeometry(0.9, 0.3, 4, 100);
 var esferaForma = new THREE.SphereGeometry(0.95);
-var cajaForma = new THREE.BoxGeometry (1, 7, 1)
+var cajaForma = new THREE.BoxGeometry (1, 6.5, 1)
 
 esferaForma.translate(0,1,0);
 troncoForma.rotateX(Math.PI/2);
-cajaForma.translate (0,-4,0);
+cajaForma.translate (0,-3,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
@@ -28,6 +28,6 @@ camara.position.z=5;
 //camara.position.y=10;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.105, window.innerHeight*.105);
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild (renderizador.domElement);
 renderizador.render ( escena, camara);
