aaa8b9854ac084cf442b31375a82b694e5ce9317
Quentiel <danielmp168@gmail.com>
2016-09-01 15:26:33 -0500

    Update ejercicio-5.js

--- rv/ejercicio-5.js
+++ rv/ejercicio-5.js
@@ -25,6 +25,7 @@ escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
+camara.position.y=10;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
