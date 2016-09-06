c6f3572cfcf778afec1109d2f4be3ea0d0a02f5b
Quentiel <danielmp168@gmail.com>
2016-09-06 14:59:07 -0500

    Update ejemplo-camaradeproyeccion.js

--- rv/ejemplo-camaradeproyeccion.js
+++ rv/ejemplo-camaradeproyeccion.js
@@ -19,8 +19,8 @@ var cubo = new THREE.Mesh (new THREE.BoxGeometry (2, 2, 2),
                                                          new THREE.MeshNormalMaterial ());
                           esfera1.position.x=5;                              
                                                         
-                          var esfera2 = new THREE.Mesh ( new THREE.SphereGeometry (1);
-                          new THREE.MeshNormalMaterial ());
+                          var esfera2 = new THREE.Mesh ( new THREE.SphereGeometry (1),
+                        				 new THREE.MeshNormalMaterial ());
                           
                           esfera2.position.x= -5;
                           esfera2.position.z=-10;
