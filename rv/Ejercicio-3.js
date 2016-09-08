7fa51b88c1653edaa93ed4868ef186db5a8ba5c5
Quentiel <danielmp168@gmail.com>
2016-09-08 15:47:19 -0500

    Update Ejercicio-3.js

--- rv/Ejercicio-3.js
+++ rv/Ejercicio-3.js
@@ -33,7 +33,7 @@ forma.computeFaceNormals();
 
 var material= new THREE.MeshNormalMaterial();
 
-var malla=new THREE.MeshBasicMaterial({map: texture}),
+var malla=new THREE.MeshBasicMaterial({map: texture});
 malla.rotateX(Math.PI/2);
 malla.rotateY(Math.PI/2);
 
