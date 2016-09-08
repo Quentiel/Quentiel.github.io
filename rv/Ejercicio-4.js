cef96bbbe929e07f63bffa41f3fd6f3bd9dd4d8d
Quentiel <danielmp168@gmail.com>
2016-09-08 17:06:44 -0500

    Update Ejercicio-4.js

--- rv/Ejercicio-4.js
+++ rv/Ejercicio-4.js
@@ -6,7 +6,7 @@ figura.lineTo(100,10);
 figura.lineTo(60,35);
 figura.moveTo(10,70);
 figura.lineTo(110,70);
-figura.lineTo(20,10);
+figura.lineTo(20,20);
 figura.lineTo(40,50);
 
 var estrella= new THREE.ShapeGeometry(figura);
@@ -16,7 +16,7 @@ var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara= new THREE.PerspectiveCamera();
-camara.position.z=250;
+camara.position.z=300;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.75,window.innerHeight*.75);
