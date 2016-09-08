4b2d4cc3bb9c72f6cee645326e41281056015967
Quentiel <danielmp168@gmail.com>
2016-09-08 17:04:58 -0500

    Update Ejercicio-4.js

--- rv/Ejercicio-4.js
+++ rv/Ejercicio-4.js
@@ -10,7 +10,7 @@ figura.lineTo(20,10);
 figura.lineTo(40,50);
 
 var estrella= new THREE.ShapeGeometry(figura);
-var material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } );
+var material = new THREE.MeshPhongMaterial( { color: 0x185489, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } );
 var malla=new THREE.Mesh(estrella, material);
 
 var escena= new THREE.Scene();
