4ca3bf5510dd177673813f222cd58317d1672077
Quentiel <danielmp168@gmail.com>
2016-09-13 14:58:25 -0500

    Create Ejemplo-13.js

---
+++ rv/Ejemplo-13.js
@@ -0,0 +1,15 @@
+var forma = new THREE.SphereGeometry(1);
+var material = new THREE.MeshLambertMaterial ({color: '#00cc00'});
+var mall = new THREE.Mesh (forma, material);
+
+var escema = new THREE.Scene ();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var lienzo = document.getElementById("basicMaterial");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, 
+                                          antialias: true});
+renderizador.setSize (600, 600);
+renderizador. render (escena, camara);
