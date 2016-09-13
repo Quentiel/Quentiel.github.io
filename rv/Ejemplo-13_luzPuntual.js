bd8366e8341df5753a6281a2b5eef9caae7d2861
Quentiel <danielmp168@gmail.com>
2016-09-13 15:20:17 -0500

    Update Ejemplo-13_luzPuntual.js

--- rv/Ejemplo-13_luzPuntual.js
+++ rv/Ejemplo-13_luzPuntual.js
@@ -16,7 +16,7 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var lienzo = document.getElementById("basicMaterial");
-var lienzo = new THREE.WebGLRenderer({canvas: lienzo, 
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, 
                                           antialias: true});
 renderizador.setSize (600, 600);
 renderizador. render (escena, camara);
